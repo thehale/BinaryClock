85db3fecb8fd51d882f1b9cf6938aadf215a6fde
Joseph Hale <me@jhale.dev>
2023-05-04 11:33:27 -0700

    chore: Add extra checks to build/bundling
    rnx-kit will check for TypeScript violations, duplicate packages, and
    cyclical dependencies when building packages, including in development
    mode.

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,13 @@
  * @format
  */
 
-module.exports = {
+const {makeMetroConfig} = require('@rnx-kit/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +20,4 @@ module.exports = {
       },
     }),
   },
-};
+});
