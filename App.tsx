d54ff0f8fd6ce57197011204727ad18770aa2beb
Joseph Hale <me@jhale.dev>
2023-05-11 23:37:39 -0700

    feat: Add a Splash Screen
    Makes for a more professional app launch experience.
    
    Thanks to `react-native-bootsplash`!
    https://github.com/zoontek/react-native-bootsplash/tree/master

--- App.tsx
+++ App.tsx
@@ -4,19 +4,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 import BinaryClockScreen from './src/pages/BinaryClockScreen';
 import BinaryClockSettingsScreen from './src/pages/BinaryClockSettingsScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
+import React from 'react';
 import Toast from 'react-native-toast-message';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={() => RNBootSplash.hide({fade: true, duration: 500})}>
       <Stack.Navigator>
         <Stack.Screen
           name="BinaryClockScreen"
--- android/app/src/main/java/com/binaryclock/MainActivity.java
+++ android/app/src/main/java/com/binaryclock/MainActivity.java
@@ -5,6 +5,10 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+// BootSplash Imports
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -31,5 +35,11 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this); // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
   }
 }
