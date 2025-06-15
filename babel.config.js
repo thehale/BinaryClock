90ad6fd6c6ea6a3981c6c1309e87093803681072
Joseph Hale <me@jhale.dev>
2025-06-14 23:13:11 -0700

    perf: Enable React Compiler

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+const ReactCompilerConfig = {}
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['babel-plugin-react-compiler', ReactCompilerConfig], // must run first!
+  ]
 };
