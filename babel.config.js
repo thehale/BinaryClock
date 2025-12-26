965f1ae03687e95180b730de706f9062b423990f
Joseph Hale <me@jhale.dev>
2025-12-26 11:53:18 -0700

    chore: Integrate `react-native-expressive` as a submodule

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,15 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ['babel-plugin-react-compiler', ReactCompilerConfig], // must run first!
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          'react-native-expressive': './lib/react-native-expressive/src',
+        },
+      },
+    ],
   ],
 };
