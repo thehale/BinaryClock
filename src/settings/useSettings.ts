751220ef35a419936e9bd9ce06b9636f8df38bda
Joseph Hale <me@jhale.dev>
2025-12-26 14:12:00 -0700

    refactor: Switch to settings helpers in `react-native-expressive`
    Binary Clock was where I first wrote these helpers, but they now live and will be maintained in `react-native-expressive`.
    
    Circle of ~life~ code.

--- src/settings/useSettings.ts
+++ src/settings/useSettings.ts
@@ -4,26 +4,36 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import { useCallback, useSyncExternalStore } from "react"
-import { SettingsStore } from "./SettingsStore"
-import { settings } from "./definitions"
+import { createSettings } from "react-native-expressive";
+import type { Settings, Setting } from "react-native-expressive";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const store = new SettingsStore(settings);
+export const definitions = {
+	/** `0`% to`1`00% brightness (e.g. `0.5` is 50% brightness) */
+	brightness: {
+		default: 1,
+		validate: (value: number) => value >= 0 && value <= 1,
+		update: async (value: number) => await AsyncStorage.setItem('brightness', value.toString()),
+		read: async () => Number(await AsyncStorage.getItem('brightness') ?? '1')
+	} satisfies Setting<number>,
 
-export async function initSettings() {
-	const entries = await Promise.all(
-		Object.entries(settings).map(async ([key, setting]) => [key, await setting.read()])
-	);
-	store.update(Object.fromEntries(entries));
-}
+	/** `0`% to`1`00% roundness (e.g. `0.5` is 50% roundness) */
+	roundness: {
+		default: 1,
+		validate: (value: number) => value >= 0 && value <= 1,
+		update: async (value: number) => await AsyncStorage.setItem('roundness', value.toString()),
+		read: async () => Number(await AsyncStorage.getItem('roundness') ?? '1')
+	} satisfies Setting<number>,
 
-export function useSettings(callerName: string = 'UnknownCaller') {
-	const subscribe = useCallback<Parameters<typeof useSyncExternalStore>[0]>((listener) => store.subscribe(callerName, listener), [callerName]);
-	const getSnapshot = useCallback(() => store.getSnapshot(), []);
-	
-	const snapshot = useSyncExternalStore(subscribe, getSnapshot);
-	const update = (updates: Partial<typeof snapshot>) => store.update(updates);
-	const resetToDefaults = useCallback(() => store.reset(), [])
-	
-	return [snapshot, update, resetToDefaults] as const;
-}
+	/** `true` shows the value of each dot, `false` does not. */
+	showHints: {
+		default: false as boolean,
+		validate: (value: boolean) => typeof value === 'boolean',
+		update: async (value: boolean) => await AsyncStorage.setItem('showHints', value.toString()),
+		read: async () => (await AsyncStorage.getItem('showHints') ?? 'false') === 'true'
+	} satisfies Setting<boolean>,
+} satisfies Settings;
+
+const { initSettings, useSettings } = createSettings(definitions);
+
+export { initSettings, useSettings };
